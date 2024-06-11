1de1c9233927543e2397eee6a59517bea313ae93
chjm44 <chc807000@naver.com>
2024-06-11 18:18:51 +0900

    최소화x

--- third/Form1.Designer.cs
+++ third/Form1.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(651, 455);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "COMBOBOX FORM";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
